0b0fa68a1c44b0759d15870de2f3f4dd079abccd
Joshua Ssemwanga <jssem001@plattsburgh.edu>
2024-03-21 13:19:50 +0300

    bonus object work

---
+++ src/00-objectball.js
@@ -0,0 +1,74 @@
+const gameObject = {
+    home: {
+      teamName: "Brooklyn Nets",
+      colors: ['Black','White'],
+      players: {
+        "Alan Anderson": {"Number": 0,
+        "Shoe": 16, "Points": 22,
+        "Rebounds": 12, "Assits": 12,
+        "Steals": 3, "Blocks": 1,
+        "Slam Dunks": 1},
+
+        "Reggie Evans": {"Number": 30,
+        "Shoe": 14, "Points": 12,
+        "Rebounds": 12, "Assits": 12,
+        "Steals": 12, "Blocks": 12,
+        "Slam Dunks": 7},
+
+        "Brook Lopez": {"Number": 11,
+        "Shoe": 17, "Points": 17,
+        "Rebounds": 19, "Assits": 10,
+        "Steals": 3, "Blocks": 1,
+        "Slam Dunks": 15},
+
+        "Mason Plumlee": {"Number": 1,
+        "Shoe": 19, "Points": 26,
+        "Rebounds": 12, "Assits": 6,
+        "Steals": 3, "Blocks": 8,
+        "Slam Dunks": 5},
+
+        "Jason Terry": {"Number": 31,
+        "Shoe": 15, "Points": 19,
+        "Rebounds": 2, "Assits": 2,
+        "Steals": 4, "Blocks": 11,
+        "Slam Dunks": 1}
+      }
+    },
+    away: {
+        teamName: "Charlotte Hornets",
+        colors: ["Turquoise","Purple"],
+        players: {
+          "Jeff Andrien": {"Number": 4,
+          "Shoe": 18, "Points": 10,
+          "Rebounds": 1, "Assits": 1,
+          "Steals": 2, "Blocks": 7,
+          "Slam Dunks": 2},
+          
+          "Bismark Biyombo": {"Number": 0,
+          "Shoe": 16, "Points": 12,
+          "Rebounds": 4, "Assits": 7,
+          "Steals": 7, "Blocks": 15,
+          "Slam Dunks": 10},
+          
+          "DeSanga Diop": {"Number": 2,
+          "Shoe": 14, "Points": 24,
+          "Rebounds": 12, "Assits": 12,
+          "Steals": 4, "Blocks": 5,
+          "Slam Dunks": 5},
+          
+          "Ben Gordon": {"Number": 8,
+          "Shoe": 15, "Points": 33,
+          "Rebounds": 3, "Assits": 2,
+          "Steals": 1, "Blocks": 1,
+          "Slam Dunks": 0},
+
+          "Brendan Haywood":{"Number": 33,
+          "Shoe": 15, "Points": 6,
+          "Rebounds": 12, "Assits": 12,
+          "Steals": 22, "Blocks": 5,
+          "Slam Dunks": 12}
+        }
+    } 
+}
+
+console.log(gameObject)
